9151b834d17a7353cc13b5e1314eea81363770fb
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2025-08-04 20:17:05 +0200

    Projektdateien hinzufügen.

---
+++ DataTableListView/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MainWindow.cs" company="Lifeprojects.de">
+//     Class: MainWindow
+//     Copyright © Lifeprojects.de 2025
+// </copyright>
+//
+// <author>Gerhard Ahrens - Lifeprojects.de</author>
+// <email>developer@lifeprojects.de</email>
+// <date>04.08.2025 20:11:52</date>
+//
+// <summary>
+// MainWindow mit Minimalfunktionen
+// </summary>
+//-----------------------------------------------------------------------
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Ribbon;
+
+namespace DataTableListView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            WeakEventManager<Window, RoutedEventArgs>.AddHandler(this, "Loaded", this.OnLoaded);
+            WeakEventManager<Window, CancelEventArgs>.AddHandler(this, "Closing", this.OnWindowClosing);
+
+            this.WindowTitel = "Minimal WPF Template";
+            this.DataContext = this;
+        }
+
+        private string _WindowTitel;
+
+        public string WindowTitel
+        {
+            get { return _WindowTitel; }
+            set
+            {
+                if (this._WindowTitel != value)
+                {
+                    this._WindowTitel = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            WeakEventManager<Button, RoutedEventArgs>.AddHandler(this.BtnCloseApplication, "Click", this.OnCloseApplication);
+
+        }
+
+        private void OnCloseApplication(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void OnWindowClosing(object sender, CancelEventArgs e)
+        {
+            e.Cancel = false;
+
+            MessageBoxResult msgYN = MessageBox.Show("Wollen Sie die Anwendung beenden?", "Beenden", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (msgYN == MessageBoxResult.Yes)
+            {
+                App.ApplicationExit();
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        #region INotifyPropertyChanged implementierung
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler == null)
+            {
+                return;
+            }
+
+            var e = new PropertyChangedEventArgs(propertyName);
+            handler(this, e);
+        }
+        #endregion INotifyPropertyChanged implementierung
+    }
+}
